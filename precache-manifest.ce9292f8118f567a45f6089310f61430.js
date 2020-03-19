703d1f04a7a5e8211ab4f88dba7c5546ae515f34
Łukasz Wiącek <lukasz.wiacek@mayeryn.com>
2020-03-19 21:55:08 +0100

    Updates

---
+++ precache-manifest.ce9292f8118f567a45f6089310f61430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc541ea5dec1f73e1dfccd3e6db7f960",
+    "url": "/treeCellApp/index.html"
+  },
+  {
+    "revision": "ecf141ac12323a093681",
+    "url": "/treeCellApp/static/css/main.882d4787.chunk.css"
+  },
+  {
+    "revision": "e91a1b2ffd6ff8571d5d",
+    "url": "/treeCellApp/static/js/2.598aded1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/treeCellApp/static/js/2.598aded1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf141ac12323a093681",
+    "url": "/treeCellApp/static/js/main.a31916a1.chunk.js"
+  },
+  {
+    "revision": "ded974b92ca47fa6469f",
+    "url": "/treeCellApp/static/js/runtime-main.d8684e0e.js"
+  }
+]);
